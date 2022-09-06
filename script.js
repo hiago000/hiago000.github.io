3bc9b7a94e180c93245ea10d96b2574b53f73bc1
hiago000 <hiago565656@hotmail.com>
2022-09-06 17:16:40 -0300

    sumario 17:16

---
+++ script.js
@@ -0,0 +1,24 @@
+/* let numero1 = Number(prompt("digite primeiro número"));
+let operacao = prompt("digite a operação, +, -, /, *");
+let numero2 = Number(prompt("digite segundo número"));
+let resultado = "";
+
+if (operacao === "+") {
+    resultado = numero1 + numero2;
+    alert(`o resultado de ${numero1} + ${numero2} = ${resultado}`)
+}
+
+if (operacao === "-") {
+    resultado = numero1 - numero2;
+    alert(resultado)
+}
+
+if (operacao === "*") {
+    resultado = numero1 * numero2;
+    alert(resultado)
+}
+
+if (operacao === "/") {
+    resultado = numero1 / numero2;
+    alert(resultado)
+} */
